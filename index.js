1dcb547fa5ebabc8f5635b8ffaed9ff61b88b103
Jenkins <jengalabscp@gmail.com>
2021-05-03 10:34:24 -0400

    Got Media Querrys working

--- index.js
+++ index.js
@@ -1,25 +1,39 @@
+/*
+    Header JS
+    Makes the slideshow for the header,
+    storing the h1 and desrcription in a array. 
+*/
 
-//When user scrolls 
-$(window).scroll(function() {
-    
-   $('.intersection').each(function(i) {
-  
-    $window = $(window);
-    var increase = i * 50
-    var newHeight = $(".intersection").outerHeight();
-    var newWidth = $(".intersection").outerWidth();
-    var distance = $(this).parent().offset().top + newHeight
 
-    if ($window.scrollTop() >= distance) {
-      $(this).addClass('sticked').css("transform", "translate(100%," + increase + "px) scale(.8)");
-      $(this).parent().css({"height": newHeight, "width": newWidth });
-   
-    }
-    else {
-      $(this).removeClass('sticked').css("transform", "translate(0px, 0px)");
-    }
-  });  
-    
-    
-    
-});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
