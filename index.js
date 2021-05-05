e7c4479ea5ca6b982bd45ec4b02e641f3c614fb4
Jenkins <jengalabscp@gmail.com>
2021-05-05 10:20:06 -0400

    Slide System Working
    Created slide system and got the pig space station rotation and text working. Now I need to create actual text to go in the slides and make ways for these systems to work on small screens .

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+"use strict";
+
 /*
     Header JS
     Makes the slideshow for the header,
@@ -5,6 +7,34 @@
 */
 
 
+
+
+
+
+/*
+    Navagation Header
+*/
+
+//Store the current slide
+let currentSlide = 0;
+
+//A two dimensional array storing header of navagation and innerhtml for aside
+let navagations = [
+    ['Welcome', '<p>nerd </p>'],
+    ['Introducing', '<p id="nerd">slides</p>']
+];
+
+
+//Move navagation slides to another number 
+function MoveNavagation(move)
+{
+    //Updates the current slide
+    currentSlide += move;
+    //Changes header text
+    document.getElementById("navHead").textContent = navagations[currentSlide][0];
+    //Changes navagation content
+    document.getElementById("navText").innerHTML = navagations[currentSlide][1];
+}
 
 
 
