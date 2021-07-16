4a01a609280da6e2b095f76d6305d1cd5850fbba
Pig <jengalabscp@gmail.com>
2021-07-16 14:35:16 -0400

    Medina Fly Portrait Work

---
+++ Pages/FlyMedina/FlyMedinaFunctions.js
@@ -0,0 +1,16 @@
+
+
+
+
+
+function openNav() {
+    document.getElementById("SideNav").style.width = "35vw";
+    document.getElementById("openbtn").style.visibility = "hidden";
+}
+
+function closeNav() {
+    document.getElementById("SideNav").style.width = "0";
+    document.getElementById("openbtn").style.visibility = "visible";
+}
+
+
